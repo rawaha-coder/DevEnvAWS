0d1783baac90f661c4f1b2e184ed30750b2ad0aa
rawaha-coder <rawaha.coder@gmail.com>
2024-10-27 12:48:07 +0100

    recreate vpc after destroy

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_vpc" "rwh_vpc" {
-    cidr_block = "1072.31.0.0/16"
-    enable_dns_hostnames = true
-    enable_dns_support = true # enable by default
+  cidr_block           = "172.31.0.0/16"
+  enable_dns_hostnames = true
+  enable_dns_support   = true # enable by default
 
-    tags = {
-      Name = "dev"
-    }
+  tags = {
+    Name = "dev"
+  }
 }
--- providers.tf
+++ providers.tf
@@ -8,7 +8,7 @@ terraform {
 }
 
 provider "aws" {
-  region     = "eu-north-1"
+  region                   = "eu-north-1"
   shared_credentials_files = ["~/.aws/credentials"]
   profile                  = "zakariae"
 }
